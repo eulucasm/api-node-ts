bb47b68c038081a4ffea95ea0e5597105c4e523b
eulucasm <lucaspoletis@gmail.com>
2021-12-06 11:19:50 -0300

    adicionando uuid e refatorando rotas

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,20 @@
 import { response, Router } from 'express';
+import { v4 as uuidV4 } from 'uuid'
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post("/categories", (req, res) => {
+categoriesRoutes.post("/", (req, res) => {
    const { name, description } = req.body;
-   categories.push({
+
+   const category = {
       name,
-      description
-   })
+      description,
+      id: uuidV4()
+   }
+
+   categories.push(category);
 
    return res.status(201).send();
 });
