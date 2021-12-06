51b24f345d171dd42b8c7573c7b6595bac69d2bb
eulucasm <lucaspoletis@gmail.com>
2021-12-06 13:34:44 -0300

    validaçẽos na rota category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,27 @@
 import { response, Router } from 'express';
-import { v4 as uuidV4 } from 'uuid'
+import { CategoryRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoryRepository();
 
-const categories = [];
 
 categoriesRoutes.post("/", (req, res) => {
    const { name, description } = req.body;
 
-   const category = {
-      name,
-      description,
-      id: uuidV4()
+   const categoryAlreadyExists = categoriesRepository.findByName(name);
+   if (categoryAlreadyExists) {
+      return res.status(400).json({ error: "Category already exists" });
    }
 
-   categories.push(category);
+   categoriesRepository.create({ name, description })
 
    return res.status(201).send();
 });
+
+categoriesRoutes.get("/", (req, res) => {
+   const all = categoriesRepository.list();
+
+   return res.json(all);
+})
 
 export { categoriesRoutes };
