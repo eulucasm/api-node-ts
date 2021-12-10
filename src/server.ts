94cdec0c2f80d22758d37de1f3c4ac91d78a4a2d
eulucasm <lucaspoletis@gmail.com>
2021-12-10 14:22:02 -0300

    iniciando o swagger na api

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+
 import { router } from "./routes/index";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(router)
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => console.log("server running!!!"));
